52078946412a4407a42183cf3a98ec901ac46b3c
Hrishii-s <hrishikesh.s.dev@gmail.com>
2025-05-19 16:30:18 +0530

    intial commit

---
+++ spirograph.py
@@ -0,0 +1,32 @@
+import turtle as t
+import random
+
+tim= t.Turtle()
+tim.shape('classic')
+t.colormode(255)
+
+
+def randcol():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0, 255)
+    clr=(r, g, b)
+    return clr
+
+
+tim.speed(100)
+def draw(size):
+    for i in range (int(360/size)):
+        tim.circle(100)
+        tim.setheading(tim.heading()+size)
+        tim.color(randcol())
+
+draw(5)
+
+
+
+
+
+
+
+
